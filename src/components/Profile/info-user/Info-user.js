d0386e82b0b81a844d6fa3229359c34638cc6de0
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-07-09 00:33:49 +0300

    ProfileStatus+local state editMode

--- src/components/Profile/info-user/Info-user.js
+++ src/components/Profile/info-user/Info-user.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Info-user.module.scss'
 import Preloader from "../../preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const InfoUser = (props) => {
     if (!props.profile) {
@@ -11,6 +12,7 @@ const InfoUser = (props) => {
             <img src={props.profile.photos.large} alt=""/>
             <div className="ava">{props.profile.fullName}</div>
             <div className="descr">{props.profile.aboutMe}</div>
+            <ProfileStatus status={"Hello my friends"}/>
         </div>
     );
 };
