dc3fbd9b735cf11841a613d4c8e4e2961a18b448
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-07-20 19:44:58 +0300

    login logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,8 @@ const Header = (props) => {
         <header className={s.header}>
             <img src={logo} alt="logo"/>
             <div className={s.loginBlock}>
-                {props.isAuth ? props.login
+                {props.isAuth
+                    ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
                     : <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./FormsControls.module.scss";
 
-const FormControl = ({input, meta, child, ...props}) => {
+const FormControl = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
