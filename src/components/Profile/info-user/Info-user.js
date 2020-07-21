08a7894e3a6eaef148db6847ce4b981ee0aa7050
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-07-21 19:36:28 +0300

    useState and useEffect in ProfileStatus

--- src/components/Profile/info-user/Info-user.js
+++ src/components/Profile/info-user/Info-user.js
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Info-user.module.scss'
 import Preloader from "../../common/preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const InfoUser = (props) => {
     if (!props.profile) {
@@ -12,7 +13,7 @@ const InfoUser = (props) => {
             <img src={props.profile.photos.large} alt=""/>
             <div className="ava">{props.profile.fullName}</div>
             <div className="descr">{props.profile.aboutMe}</div>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     );
 };
---
+++ src/components/Profile/info-user/ProfileStatusWithHooks.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                {/*{console.log(this.props.status)}*/}
+                <span onDoubleClick={activateEditMode}>{props.status || "-------"}</span>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode}
+                       value={status}/>
+            </div>
+            }
+        </div>
+    )
+
+}
+
+export default ProfileStatusWithHooks;
